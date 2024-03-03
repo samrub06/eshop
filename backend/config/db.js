733227889c7341418f806a264a919332272cc4f4
samuel <samrub06@gmail.com>
2024-03-03 13:32:14 +0200

    fix url mongo

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
-    process.exit(1)
+    process.exit(1);
   }
 };
 
-export default connectDB
+export default connectDB;
